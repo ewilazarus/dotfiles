f80a9b4952e7303d440cc19c77c84cacba302d25
Gabriel Lima <ewilazarus@gmail.com>
2016-06-05 05:20:04 -0300

    Changes in macbook

--- fish/config.fish
+++ fish/config.fish
@@ -16,23 +16,21 @@ eval (python -m virtualfish)
 
 # Aliases
 
-alias ll='ls -alhF --group-directories-first'
-alias la='la -A --group-directories-first'
-alias l='ls -CF --group-directories-first'
+alias ll="ls -alhF"
+alias la="la -A"
+alias l="ls -CF"
 
-alias tfak='ssh limag@login.informatik.uni-freiburg.de'
-alias tmux='tmux -2'  # enables 256 color support for tmux
-alias ack='ack-grep'
+alias tmux="tmux -2"  # enables 256 color support for tmux
 
-alias tmat='tmux -2 attach -t'
-alias tmks='tmux -2 kill-server'
-alias tml='tmuxp load'
+alias tmat="tmux -2 attach -t"
+alias tmks="tmux -2 kill-server"
+alias tml="tmuxp load"
 
-alias ev='vim ~/.vimrc'
-alias ez='vim ~/.zshrc'
-alias et='vim ~/.tmux.conf'
-alias ep='vim ~/.dotfiles/etc/packages.txt'
+alias ev="vim ~/.vimrc"
+alias ez="vim ~/.zshrc"
+alias et="vim ~/.tmux.conf"
+alias ep="vim ~/.dotfiles/etc/packages.txt"
 
-alias nopyc='rm *.pyc'
+alias nopyc="rm *.pyc"
 
-alias cb='xclip -selection c -i'  # thanks to firstdoit
+alias cb="xclip -selection c -i"  # thanks to firstdoit
