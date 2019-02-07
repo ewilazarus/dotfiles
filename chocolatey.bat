e18e230355e59948e2fb3c1b25baa2282bacc0e7
Gabriel Lima <gvclima@gmail.com>
2019-02-07 15:36:01 +0000

    Add chocolatey.bat

---
+++ chocolatey.bat
@@ -0,0 +1,11 @@
+choco install googlechrome -y
+choco install postman -y
+choco install notepadplusplus -y
+choco install vim -y
+choco install conemu -y
+choco install sharpkeys -y
+choco install 7zip -y
+choco install fiddler -y
+choco install openvpn -y
+choco install dbeaver -y
+
